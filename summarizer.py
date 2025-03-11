5cad0529b689a21e139eab342ccd4f507cc3dbfb
tekemehmet <tekemehmet@yahoo.com>
2025-03-10 20:57:39 -0400

    Hastag added

--- summarizer.py
+++ summarizer.py
@@ -1,17 +1,28 @@
 from openai import OpenAI
 import os
 from dotenv import load_dotenv
+import random 
 
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+
 def summarize_and_translate(article):
-    prompt = f"Haberi sadece Türkçe olarak tek cümlede özetle: {article['title']}"
+    prompt = f"""
+    Haberi sadece Türkçe olarak iki cümlede özetle. 
+    İlk cümlede haberin en önemli detayını açıkla. 
+    İkinci cümlede MTB severler için neden önemli olduğunu belirt. 
+    Son olarak, en alakalı 2 hashtag'i belirle ve ekle.
     
+    Haber başlığı: {article['title']}
+    """
+     
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[{"role": "user", "content": prompt}]
         
     )
     
-    return response.choices[0].message.content
+    return  response.choices[0].message.content
+
+    
